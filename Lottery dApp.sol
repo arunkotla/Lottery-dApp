9641c57bf41b5e317e17315ad842daaa44b3d97c
arunkotla <arunkkotla1998@gmail.com>
2023-05-09 13:19:28 +0530

    adding conditions & picking winner

--- Lottery dApp.sol
+++ Lottery dApp.sol
@@ -16,13 +16,28 @@ contract lottery{
     }
 
     function Apply(string memory _name , address _addr)  payable public {
-        require(Applications.length == 0, "a");
         require(hasApplied[_name] == address(0), "b"); 
         require(msg.value == 2 ether, "must pay 2 ether to enter");       
 
         Applications.push(payable(msg.sender));
         names.push(_name);
         hasApplied[_name] = msg.sender;
-        
+    }
+
+    function random() private view returns(uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, Applications.length)));
+    }
+
+    function pickWinner () public  {
+
+        require(msg.sender ==owner, "only owner can call this function");
+
+        uint index = random() % Applications.length;
+        Applications[index].transfer(address(this).balance);
+        Applications = new address payable[](0);
+    }
+
+    function getApplications () public view returns (address payable[] memory ) {
+        return Applications;
     }
 }
